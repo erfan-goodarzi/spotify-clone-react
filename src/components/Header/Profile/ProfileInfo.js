a07513e8104fa984f1863255ad0acd17c03db369
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-11-20 17:47:53 +0330

    Fetching user information and dispatch it

--- src/components/Header/Profile/ProfileInfo.js
+++ src/components/Header/Profile/ProfileInfo.js
@@ -1,14 +1,23 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useDispatch, useSelector } from "react-redux";
-// import { getTokenFromResponse } from "../../../container/config-spotify";
-// import { GetUserInfo } from "../../../redux/SpotifySlice";
-
-
+import { getTokenFromResponse } from "../../../container/config-spotify";
+import { GetUserInfo } from "../../../redux/SpotifySlice";
+import useAxios from "axios-hooks";
+import axios from "axios";
 const ProfileInfo = () => {
   const user = useSelector((state) => state.spotify.userInfo);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios({
+      url: "https://api.spotify.com/v1/me",
+      headers: {
+        Authorization: "Bearer " + getTokenFromResponse().access_token,
+      },
+    }).then((res) => dispatch(GetUserInfo(res.data.id)));
+  }, [dispatch]);
 
   return (
     <>
