fab669a174ae6a648ff63910efbbba743dfa9799
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-12-04 16:09:14 +0330

    chore: create top cover music

---
+++ src/components/main/TopBanner.jsx
@@ -0,0 +1,28 @@
+import Box from "@mui/material/Box";
+const Topbanner = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: 1035,
+          height: "43vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          borderRadius: "20px",
+          mt: 4,
+          transition: "all 0.3s ease-in-out",
+
+          backgroundImage:
+            "url(https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/165899486/original/4d086ab0c297311d6684fa0f8f21fc43c991945d/design-a-music-cover-artwork-album-cover-design.jpg)",
+          "&:hover": {
+            transition: "all 0.3s ease-in-out",
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default Topbanner;
--- src/components/main/home/HomePage.jsx
+++ src/components/main/home/HomePage.jsx
@@ -1,25 +1,28 @@
 import Grid from "@mui/material/Grid";
+import Topbanner from "../TopBanner";
 
 const Homepage = () => {
   return (
-    <Grid
-      sx={{ mt: 1 }}
-      container
-      direction="column"
-      justifyContent="space-evenly"
-      alignItems="center"
-      spacing={12}
-    >
-      <Grid item xs={8}>
-        Top banner
+    <>
+      <Grid
+        sx={{ pl: 6 }}
+        container
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="right"
+        spacing={12}
+      >
+        <Grid item xs={8}>
+          <Topbanner />
+        </Grid>
+        <Grid item xs={8}>
+          main
+        </Grid>
+        <Grid item xs={8}>
+          Latest Releases
+        </Grid>
       </Grid>
-      <Grid item xs={8}>
-        main
-      </Grid>
-      <Grid item xs={8}>
-        Latest Releases
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
